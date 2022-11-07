4b54ae8572dd8854fccf08bd1f311b2593e553e6
Oliver S <oliver@skjevesland.com>
2022-11-07 16:28:15 +0100

    added country filter

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
   const [rqValue, setRqValue] = useState([80, 110]);
   const [rqOrder, setRqOrder] = useState(true);
   const [carTag, setCarTag] = useState([]);
+  const [carCountryValue, setCarCountryValue] = useState([]);
   // { Array.isArray(car.make) ? car.make[0] : car.make }
   const numOfCars = 20;
   const carTags = [];
@@ -50,7 +51,8 @@ function App() {
       regexSearch.exec(car.model)
       ) &&
       car.rq <= rqValue[1] && car.rq >= rqValue[0] &&
-      (carTag.length > 0 ? carTag.some(elem => car.tags.includes(elem)) : true)
+      (carTag.length > 0 ? carTag.some(elem => car.tags.includes(elem)) : true) &&
+        (carCountryValue.length > 0 ? carCountryValue.some(elem => elem.code === car.country) : true)
     })
   }
 
@@ -62,9 +64,6 @@ function App() {
           if (!carTags.includes(tag)) {
             carTags.push(tag);
           }
-          if(car.tags.includes("Unqiue")) {
-            console.log(`CAR THAT HAS TYPOS ${car.make}| ${car.model}`)
-          }
         })
       }
     })
@@ -72,7 +71,7 @@ function App() {
 
   useEffect(() => {
     getAllTags()
-    console.log("CAR TAGS --->", carTags)
+    // console.log("CAR TAGS --->", carTags)
   }, [])
 
   return (
@@ -81,7 +80,7 @@ function App() {
         <img src={Logo} style={{ width: "90%", backgroundColor: "white", border: "5px solid white" }} />
       </Box>
 
-      <CarFilter carTag={carTag} setCarTag={setCarTag} carTags={carTags} setRqOrder={setRqOrder} rqOrder={rqOrder} setSearch={setSearch} search={search} setRqValue={setRqValue} rqValue={rqValue} setPage={setPage} />
+      <CarFilter carCountryValue={carCountryValue} setCarCountryValue={setCarCountryValue} carTag={carTag} setCarTag={setCarTag} carTags={carTags} setRqOrder={setRqOrder} rqOrder={rqOrder} setSearch={setSearch} search={search} setRqValue={setRqValue} rqValue={rqValue} setPage={setPage} />
 
       <Cards rqOrder={rqOrder} filteredCars={filteredCars} page={page} numOfCars={numOfCars} />
 
