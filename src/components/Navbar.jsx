98b40b9919fb6f533c21940d8ab438da417a1fca
Oliver S <oliver@skjevesland.com>
2023-04-01 13:40:04 +0100

    FrontPage, added title to front page, added 4 front page items that sends the user to different pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,95 +13,107 @@ import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
 import Link from '@mui/material/Link';
 
-import Logo from '../logowhite.png'
+import Logo from '../images/logowhite.png';
 
-const pages = ['Tune Calculator',
-  // 'Creation', 'Tracks', 'Packs'
+const pages = [
+	'Tune Calculator',
+	// 'Creation', 'Tracks', 'Packs'
 ];
 const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 const Navbar = () => {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
+	const [anchorElNav, setAnchorElNav] = React.useState(null);
+	const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+	const handleOpenNavMenu = (event) => {
+		setAnchorElNav(event.currentTarget);
+	};
+	const handleOpenUserMenu = (event) => {
+		setAnchorElUser(event.currentTarget);
+	};
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+	const handleCloseNavMenu = () => {
+		setAnchorElNav(null);
+	};
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null);
+	};
 
-  return (
-    <AppBar color="success" position="absolute" top="0" left="0">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Link href="/" sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
-            <img src={Logo} style={{ width: "20rem" }} />
-          </Link>
+	return (
+		<AppBar
+			color='success'
+			position='absolute'
+			top='0'
+			left='0'>
+			<Container maxWidth='xl'>
+				<Toolbar disableGutters>
+					<Link
+						href='/'
+						sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }}>
+						<img
+							src={Logo}
+							style={{ width: '20rem' }}
+						/>
+					</Link>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Box sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, width: "70%" }}>
-            <img src={Logo} style={{ width: "20rem" }} />
-          </Box>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                href={`/${page.split(" ").join("-").toLowerCase()}`}
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+					<Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+						<IconButton
+							size='large'
+							aria-label='account of current user'
+							aria-controls='menu-appbar'
+							aria-haspopup='true'
+							onClick={handleOpenNavMenu}
+							color='inherit'>
+							<MenuIcon />
+						</IconButton>
+						<Menu
+							id='menu-appbar'
+							anchorEl={anchorElNav}
+							anchorOrigin={{
+								vertical: 'bottom',
+								horizontal: 'left',
+							}}
+							keepMounted
+							transformOrigin={{
+								vertical: 'top',
+								horizontal: 'left',
+							}}
+							open={Boolean(anchorElNav)}
+							onClose={handleCloseNavMenu}
+							sx={{
+								display: { xs: 'block', md: 'none' },
+							}}>
+							{pages.map((page) => (
+								<MenuItem
+									key={page}
+									onClick={handleCloseNavMenu}>
+									<Typography textAlign='center'>{page}</Typography>
+								</MenuItem>
+							))}
+						</Menu>
+					</Box>
+					<Box
+						sx={{ display: { xs: 'flex', md: 'none' }, mr: 1, width: '70%' }}>
+						<img
+							src={Logo}
+							style={{ width: '20rem' }}
+						/>
+					</Box>
+					<Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+						{pages.map((page) => (
+							<Button
+								href={`/${page.split(' ').join('-').toLowerCase()}`}
+								key={page}
+								onClick={handleCloseNavMenu}
+								sx={{ my: 2, color: 'white', display: 'block' }}>
+								{page}
+							</Button>
+						))}
+					</Box>
 
-          <Box sx={{ display: "flex", gap: "1rem", flexGrow: 0 }}>
-
-            {/* <Tooltip title="Open settings">
+					<Box sx={{ display: 'flex', gap: '1rem', flexGrow: 0 }}>
+						{/* <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
@@ -128,15 +140,28 @@ const Navbar = () => {
                 </MenuItem>
               ))}
             </Menu> */}
-            <Tooltip title="Join the Discord server!">
-              <Link id="discordLink" href="https://discord.gg/p3UbVP9BxB" target="_blank" sx={{ display: "flex", justifyContent: "center", alignItems: "center", transition: ".2s" }}>
-                <img alt="" style={{ width: 40, borderRadius: 50 }} src="src/discord-v2-svgrepo-com.svg" />
-              </Link>
-            </Tooltip>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
-  );
-}
+						<Tooltip title='Join the Discord server!'>
+							<Link
+								id='discordLink'
+								href='https://discord.gg/p3UbVP9BxB'
+								target='_blank'
+								sx={{
+									display: 'flex',
+									justifyContent: 'center',
+									alignItems: 'center',
+									transition: '.2s',
+								}}>
+								<img
+									alt=''
+									style={{ width: 40, borderRadius: 50 }}
+									src='src/discord-v2-svgrepo-com.svg'
+								/>
+							</Link>
+						</Tooltip>
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
+};
 export default Navbar;
